5432b043090723c34088c8e2cc37ff7dfade034e
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-01-04 15:28:31 +0530

    working with models

--- tasks/models.py
+++ tasks/models.py
@@ -1,20 +1,35 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
 class todo(models.Model):
-    TASK_PRIORITY = (
-        ('p1', 'priority-1'),
-        ('p2', 'priority-2'),
-        ('p3', 'priority-3'),
-        ('p4', 'priority-4')
-    )
-    todo_id = models.AutoField(max_length=10, primary_key=True, )
-    title = models.CharField(max_length=30)
+    todo_id = models.AutoField(max_length=10, primary_key=True)
+    title = models.CharField(max_length=30, null=False)
     complete = models.BooleanField(default=False)
-    priority = models.CharField(max_length=10, choices=TASK_PRIORITY)
+    priority = models.ForeignKey(to=priority, on_delete=models.CASCADE)
 
 
     def __str__(self):
         """Unicode representation of MODELNAME."""
         return self.name
+
+
+# ? should I do this as follows or incorporate that to the todo model itself as choice[char-field]
+class priority(models.Model):
+  TASK_PRIORITY = [
+        ('p1', 'Red'),
+        ('p2', 'Orange'),
+        ('p3', 'Blue'),
+        ('p4', 'White')
+  ]
+  priority = models.CharField(max_length=10, blank=True, choices=TASK_PRIORITY)
+
+class dueDate(models.Model):
+  current_time = datetime.datetime.now() 
+  today = current_time.today().date()
+  tomorrrow = datetime.date.today() + datetime.timedelta(days=1)
+
+  # * TODO: Complete this model configuration and make migrations and migrate the datebase
+  # ? Should I put due dates to the task
+  
---
+++ test.py
@@ -0,0 +1,5 @@
+import datetime
+
+current_time = datetime.datetime.now()
+
+print(datetime.date.today() + datetime.timedelta(days=1))
